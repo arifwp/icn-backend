85d29c56a259b68a7d7f8c836ac21f0e66ec67cf
Arif Wahyu <arifwahyu696@gmail.com>
2025-10-23 04:23:20 +0700

    add jwt guard

--- src/common/guards/jwt-auth/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth/jwt-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(process.env.JWT_SECRET) {}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
