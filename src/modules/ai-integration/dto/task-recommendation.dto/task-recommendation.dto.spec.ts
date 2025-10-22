44200222f4d0d554d1ae93e466d69b93bdfb8abc
Arif Wahyu <arifwahyu696@gmail.com>
2025-10-23 04:22:04 +0700

    remove files

--- src/modules/ai-integration/dto/task-recommendation.dto/task-recommendation.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TaskRecommendationDto } from './task-recommendation.dto';
-
-describe('TaskRecommendationDto', () => {
-  it('should be defined', () => {
-    expect(new TaskRecommendationDto()).toBeDefined();
-  });
-});
